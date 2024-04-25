99cb43a8a1914110ee72c03337087b8dcd72de24
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-04-25 05:10:41 +0300

    add structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import Home from 'pages/Home/Home';
+import MovieDetails from 'pages/MovieDetails/MovieDetails';
+import Movies from 'pages/Movies/Movies';
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/movies" element={<Movies />}></Route>
+      <Route path="/movies-details" element={<MovieDetails />}></Route>
+    </Routes>
   );
 };
