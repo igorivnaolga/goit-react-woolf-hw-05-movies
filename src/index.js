ddb11bdce8011990346ed7d7ef0205791d6748db
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-06-07 06:26:59 +0300

    update package.json

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-woolf-hw-05-movies">
+  <BrowserRouter basename="/movies">
     <React.StrictMode>
       <App />
     </React.StrictMode>
